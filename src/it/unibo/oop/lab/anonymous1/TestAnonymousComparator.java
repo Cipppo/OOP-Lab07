dd38ca64d395bacce6fb3e7462930ebb9f7ad41b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-09 15:17:13 +0100

    Sorted Denzel Washington Following list incrementally by age

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,14 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        Collections.sort(denzelUsers, new Comparator<User>() {
+
+			@Override
+			public int compare(User o1, User o2) {
+				return Integer.compare(o1.getAge(), o2.getAge());
+			}
+        	
+        });
         /*
          * expected Result
          */
